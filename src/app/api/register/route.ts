fcd70655e3e7124847af6fa7bdbd4dab9bc08b3e
raishudesu <bacaltosbaryshnikov@gmail.com>
2023-12-29 12:29:34 +0800

    test update

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -19,7 +19,7 @@ export async function POST(req: Request) {
     );
   } catch (error) {
     return NextResponse.json(
-      { message: "Something went wrong. Please try again", error },
+      { message: "Something went wrong.", error },
       { status: 500 }
     );
   }
